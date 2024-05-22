9405eb1df0637e6c85a8da90b4f2a414a71963e8
Nodens-Ancient <60713065+Nodens-Ancient@users.noreply.github.com>
2024-05-22 20:00:43 +0300

    added userService && orderService

---
+++ TestTask/TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var minQuantity = 1;
+            var order = await _context.Orders
+                                    .OrderByDescending(o => o.CreatedAt)
+                                    .FirstOrDefaultAsync(o => o.Quantity > minQuantity);
+            return order;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var orders = await _context.Orders
+                                      .Where(o => o.User.Status == Enums.UserStatus.Active)
+                                      .OrderByDescending(o => o.CreatedAt)
+                                      .ToListAsync();
+            return orders;
+        }
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var yearOfCreation = 2003;
+            var userQuery = await _context.Users
+                                .Select(user
+                                            => new { user, 
+                                                     totalOrderSum = user.Orders
+                                                                             .Where(o => o.Status == Enums.OrderStatus.Delivered && o.CreatedAt.Year == yearOfCreation)
+                                                                             .Sum(o => o.Quantity * o.Price) })
+                                .OrderByDescending(data => data.totalOrderSum)
+                                .FirstOrDefaultAsync();
+            return  userQuery.user;
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            var yearOfCreation = 2010;
+            var users = _context.Users
+                                    .Where(u => u.Orders.FirstOrDefault(o => o.Status == Enums.OrderStatus.Paid 
+                                                                            && o.CreatedAt.Year == yearOfCreation) != null)
+                                    .ToListAsync();
+            return users;
+        }
+    }
+}
